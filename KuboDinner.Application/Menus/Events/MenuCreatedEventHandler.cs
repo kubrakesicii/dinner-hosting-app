f4876da59c14ccb23b95c4f9345af61a14d93eba
Kübra <kubrakesicii@github.com>
2025-03-16 16:24:10 +0300

    Domain Event Handler is implemented

---
+++ KuboDinner.Application/Menus/Events/MenuCreatedEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using KuboDinner.Domain.Menu.Events;
+using MediatR;
+
+namespace KuboDinner.Application.Menus.Events
+{
+    public class MenuCreatedEventHandler : INotificationHandler<MenuCreated>
+    {
+        public Task Handle(MenuCreated notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
